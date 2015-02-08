d31a9b4798f8e3128bfb82f09558b48e5d63e75d
brleung <brleung@zynga.com>
2015-02-07 17:51:06 -0800

    Added swiping & removed auto transition
    - removed auto transition after sign out
    - added swiping to intro

---
+++ ClickBox/firstViewController.swift
@@ -0,0 +1,37 @@
+//
+//  firstViewController.swift
+//  ClickBox
+//
+//  Created by Bryan Leung on 2/7/15.
+//  Copyright (c) 2015 Bryan Leung. All rights reserved.
+//
+
+import UIKit
+
+class firstViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.contentSize = CGSize(width: 960, height: scrollView.bounds.size.height)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
